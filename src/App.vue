65fd3c57b3ff2f200cb5534183d276360f5ca00b
Ariun_Erdene <ariun-erdene@ailab.mn>
2022-10-24 08:22:59 +0800

    [CREATED] initialized project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Token:
+    <button @click="getToken">Get token</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    async getToken() {
+      await axios.post("https://us-central1-icapital-oauth2-firebase.cloudfunctions.net/tokeninfo")
+          .then(response => console.log(response)).catch(error => console.error(error))
+    }
+  }
+}
+</script>
